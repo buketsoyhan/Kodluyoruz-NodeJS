58132501c0a5cac290f226e17f8b2350088aea03
Buket Soyhan <buketsoyhan1998@outlook.com>
2022-06-12 00:08:29 +0300

    Created my server

---
+++ My Server/index.js
@@ -0,0 +1,27 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>INDEX SAYFASI</h1>");
+  } else if (url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>HAKKIMDA SAYFASI</h1>");
+  } else if (url === "/contact") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.write("<h1>İLETİŞİM SAYFASI</h1>");
+  } else {
+    res.writeHead(404, { "Content-Type": "text/html" });
+    res.write("<h1>404 SAYFA BULUNAMADI</h1>");
+  }
+
+  res.end();
+});
+
+const port = 5000;
+
+server.listen(port, () => {
+  console.log(`Sunucu port ${port} de başlatıldı.`);
+});
